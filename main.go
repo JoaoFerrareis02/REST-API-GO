a34ddfe33dbda302767f8137f0bc62cf499826b9
João Victor Ferrareis Ribeiro <joaovito2002@gmail.com>
2024-06-13 15:17:07 -0300

    Registing a POST Route

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"net/http"
 
+	"github.com/JoaoFerrareis02/REST-API-GO/models"
 	"github.com/gin-gonic/gin"
 )
 
@@ -10,12 +11,33 @@ func main() {
 
 	server := gin.Default()
 
-	server.GET("/events", getEvents) // GET /events
+	server.GET("/events", getEvents)    // GET /events
+	server.POST("/events", createEvent) // POST /events
 
 	server.Run(":8080") // localhost:8080
 
 }
 
 func getEvents(context *gin.Context) {
-	context.JSON(http.StatusOK, gin.H{"message": "Hello!"})
+
+	events := models.GetAllEvents()
+	context.JSON(http.StatusOK, events)
+	// context.JSON(http.StatusOK, gin.H{"message": "Hello!"})
+}
+
+func createEvent(context *gin.Context) {
+
+	var event models.Event
+	err := context.ShouldBindJSON(&event)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse data."})
+		return
+	}
+
+	event.ID = 1
+	event.UserID = 1
+
+	context.JSON(http.StatusCreated, gin.H{"message": "Event created!", "event": event})
+
 }
