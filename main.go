47acc1a3177f179d1dbd8602c064a9265bb2ed7a
João Victor Ferrareis Ribeiro <joaovito2002@gmail.com>
2024-06-13 14:58:08 -0300

    Installing the Gin Framework & Setting Up A First Route & Handling a First Request

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+
+	server := gin.Default()
+
+	server.GET("/events", getEvents) // GET /events
+
+	server.Run(":8080") // localhost:8080
+
+}
+
+func getEvents(context *gin.Context) {
+	context.JSON(http.StatusOK, gin.H{"message": "Hello!"})
+}
