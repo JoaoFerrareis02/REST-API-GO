f3db2e3cd84cc5e9cdc9a217d7de8307ba3701be
João Victor Ferrareis Ribeiro <joaovito2002@gmail.com>
2024-06-14 13:24:05 -0300

    Cancelling Registrations

--- routes/registers.go
+++ routes/registers.go
@@ -36,4 +36,30 @@ func registerForEvent(context *gin.Context) {
 
 }
 
-func cancelRegistration(context *gin.Context) {}
+func cancelRegistration(context *gin.Context) {
+
+	userId := context.GetInt64("userId")
+	eventId, err := strconv.ParseInt(context.Param("id"), 10, 64)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse event id."})
+		return
+	}
+
+	event, err := models.GetEventById(eventId)
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not fetch event. Try again later."})
+		return
+	}
+
+	err = event.CancelRegistration(userId)
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not cancel registration. Try again later."})
+		return
+	}
+
+	context.JSON(http.StatusOK, gin.H{"message": "Cancelled!"})
+
+}
