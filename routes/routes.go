df66e27173c36a27ab71f2aeb94d11858a2ac39c
João Victor Ferrareis Ribeiro <joaovito2002@gmail.com>
2024-06-13 22:39:01 -0300

    Deleting Events

--- routes/routes.go
+++ routes/routes.go
@@ -4,9 +4,10 @@ import "github.com/gin-gonic/gin"
 
 func RegisterRoutes(server *gin.Engine) {
 
-	server.GET("/events", getEvents)       // GET /events
-	server.GET("/events/:id", getEvent)    // GET /events/:id
-	server.POST("/events", createEvent)    // POST /events
-	server.PUT("/events/:id", updateEvent) // PUT /events/:id
+	server.GET("/events", getEvents)          // GET /events
+	server.GET("/events/:id", getEvent)       // GET /events/:id
+	server.POST("/events", createEvent)       // POST /events
+	server.PUT("/events/:id", updateEvent)    // PUT /events/:id
+	server.DELETE("/events/:id", deleteEvent) // DELETE /events/:id
 
 }
