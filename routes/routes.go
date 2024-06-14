3ebb591d9d4d23480f87ac9879a119ca53c48c0d
João Victor Ferrareis Ribeiro <joaovito2002@gmail.com>
2024-06-13 22:00:40 -0300

    Register an 'Update Event' Route & Handler

--- routes/routes.go
+++ routes/routes.go
@@ -4,8 +4,9 @@ import "github.com/gin-gonic/gin"
 
 func RegisterRoutes(server *gin.Engine) {
 
-	server.GET("/events", getEvents)    // GET /events
-	server.GET("/events/:id", getEvent) // GET /events/:id
-	server.POST("/events", createEvent) // POST /events
+	server.GET("/events", getEvents)       // GET /events
+	server.GET("/events/:id", getEvent)    // GET /events/:id
+	server.POST("/events", createEvent)    // POST /events
+	server.PUT("/events/:id", updateEvent) // PUT /events/:id
 
 }
