d6a7a1f89bfc9703f86d82dda4a5ebfcb8dfb329
João Victor Ferrareis Ribeiro <joaovito2002@gmail.com>
2024-06-14 08:47:26 -0300

    Adding User Signup

---
+++ routes/users.go
@@ -0,0 +1,29 @@
+package routes
+
+import (
+	"net/http"
+
+	"github.com/JoaoFerrareis02/REST-API-GO/models"
+	"github.com/gin-gonic/gin"
+)
+
+func signup(context *gin.Context) {
+
+	var user models.User
+	err := context.ShouldBindJSON(&user)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse data."})
+		return
+	}
+
+	err = user.Save()
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not save user. Try again later."})
+		return
+	}
+
+	context.JSON(http.StatusCreated, gin.H{"message": "User saved successfully!"})
+
+}
